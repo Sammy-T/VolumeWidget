5fa53776c9f8892ada77922ad1b9cdda6378b010
Sammy-T <cyam9019@aim.com>
2019-07-22 17:30:32 -0400

    Created Media Volume tests
    Created tests which send multiple increase and decrease volume broadcasts.

---
+++ app/src/androidTest/java/sammyt/volumewidget/VolumeReceiverTest.java
@@ -0,0 +1,89 @@
+package sammyt.volumewidget;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class VolumeReceiverTest {
+
+    private final String LOG_TAG = this.getClass().getSimpleName();
+    
+    private Context appContext = InstrumentationRegistry.getTargetContext();
+
+    @Test
+    public void testMediaVolumeDecrease() {
+        Intent testIntent = new Intent(appContext, AdjustVolumeReceiver.class);
+
+        testIntent.putExtra(AdjustVolumeReceiver.VOLUME_TYPE_EXTRA, AdjustVolumeReceiver.MEDIA_VOLUME);
+        testIntent.putExtra(AdjustVolumeReceiver.VOLUME_DIRECTION, AdjustVolumeReceiver.VOLUME_DECREASE);
+
+        AudioManager audioManager = (AudioManager) appContext.getSystemService(Context.AUDIO_SERVICE);
+
+        int beforeValue = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        appContext.sendBroadcast(testIntent);
+
+        for(int i = 0; i < 20; i++){
+            appContext.sendBroadcast(testIntent);
+        }
+
+        try{
+            Thread.sleep(3000); // Delay
+        }catch(InterruptedException e){
+            Log.e(LOG_TAG, e.getMessage(), e);
+        }
+
+        int afterValue = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        if(beforeValue > 0) {
+            Log.i(LOG_TAG, "Decreased from " + beforeValue + " to " + afterValue);
+            assertTrue(beforeValue > afterValue);
+        }else{
+            Log.i(LOG_TAG, "Decreased from 0 volume. Value remains the same.");
+            assertEquals(beforeValue, afterValue);
+        }
+    }
+
+    @Test
+    public void testMediaVolumeIncrease() {
+        Intent testIntent = new Intent(appContext, AdjustVolumeReceiver.class);
+
+        testIntent.putExtra(AdjustVolumeReceiver.VOLUME_TYPE_EXTRA, AdjustVolumeReceiver.MEDIA_VOLUME);
+        testIntent.putExtra(AdjustVolumeReceiver.VOLUME_DIRECTION, AdjustVolumeReceiver.VOLUME_INCREASE);
+
+        AudioManager audioManager = (AudioManager) appContext.getSystemService(Context.AUDIO_SERVICE);
+
+        int beforeValue = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        appContext.sendBroadcast(testIntent);
+
+        for(int i = 0; i < 20; i++){
+            appContext.sendBroadcast(testIntent);
+        }
+
+        try{
+            Thread.sleep(3000); // Delay
+        }catch(InterruptedException e){
+            Log.e(LOG_TAG, e.getMessage(), e);
+        }
+
+        int afterValue = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        if(beforeValue < audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)) {
+            Log.i(LOG_TAG, "Increased from " + beforeValue + " to " + afterValue);
+            assertTrue(beforeValue < afterValue);
+        }else{
+            Log.i(LOG_TAG, "Increased from max volume. Value remains the same.");
+            assertEquals(beforeValue, afterValue);
+        }
+    }
+}
