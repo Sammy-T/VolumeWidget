2eb49bf3606577f44abcac719178855d9b9efbab
Sammy-T <cyam9019@aim.com>
2019-07-22 23:44:18 -0400

    Added Ringer Volume Tests
    Tests multiple increase and decrease requests on the ringer stream.

--- app/src/androidTest/java/sammyt/volumewidget/VolumeReceiverTest.java
+++ app/src/androidTest/java/sammyt/volumewidget/VolumeReceiverTest.java
@@ -1,5 +1,6 @@
 package sammyt.volumewidget;
 
+import android.app.Instrumentation;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
@@ -7,6 +8,8 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,7 +19,7 @@ import static org.junit.Assert.*;
 public class VolumeReceiverTest {
 
     private final String LOG_TAG = this.getClass().getSimpleName();
-    
+
     private Context appContext = InstrumentationRegistry.getTargetContext();
 
     @Test
@@ -45,10 +48,10 @@ public class VolumeReceiverTest {
         int afterValue = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
 
         if(beforeValue > 0) {
-            Log.i(LOG_TAG, "Decreased from " + beforeValue + " to " + afterValue);
+            Log.i(LOG_TAG, "Decreased from " + beforeValue + " to " + afterValue + " MEDIA");
             assertTrue(beforeValue > afterValue);
         }else{
-            Log.i(LOG_TAG, "Decreased from 0 volume. Value remains the same.");
+            Log.i(LOG_TAG, "Decreased from 0 volume. Value remains the same. MEDIA");
             assertEquals(beforeValue, afterValue);
         }
     }
@@ -79,10 +82,78 @@ public class VolumeReceiverTest {
         int afterValue = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
 
         if(beforeValue < audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)) {
-            Log.i(LOG_TAG, "Increased from " + beforeValue + " to " + afterValue);
+            Log.i(LOG_TAG, "Increased from " + beforeValue + " to " + afterValue + " MEDIA");
             assertTrue(beforeValue < afterValue);
         }else{
-            Log.i(LOG_TAG, "Increased from max volume. Value remains the same.");
+            Log.i(LOG_TAG, "Increased from max volume. Value remains the same. MEDIA");
+            assertEquals(beforeValue, afterValue);
+        }
+    }
+
+    @Test
+    public void testRingerVolumeDecrease() {
+        Intent testIntent = new Intent(appContext, AdjustVolumeReceiver.class);
+
+        testIntent.putExtra(AdjustVolumeReceiver.VOLUME_TYPE_EXTRA, AdjustVolumeReceiver.RING_VOLUME);
+        testIntent.putExtra(AdjustVolumeReceiver.VOLUME_DIRECTION, AdjustVolumeReceiver.VOLUME_DECREASE);
+
+        AudioManager audioManager = (AudioManager) appContext.getSystemService(Context.AUDIO_SERVICE);
+
+        int beforeValue = audioManager.getStreamVolume(AudioManager.STREAM_RING);
+
+        appContext.sendBroadcast(testIntent);
+
+        for(int i = 0; i < 20; i++){
+            appContext.sendBroadcast(testIntent);
+        }
+
+        try{
+            Thread.sleep(3000); // Delay
+        }catch(InterruptedException e){
+            Log.e(LOG_TAG, e.getMessage(), e);
+        }
+
+        int afterValue = audioManager.getStreamVolume(AudioManager.STREAM_RING);
+
+        if(beforeValue > 0) {
+            Log.i(LOG_TAG, "Decreased from " + beforeValue + " to " + afterValue + " RINGER");
+            assertTrue(beforeValue > afterValue);
+        }else{
+            Log.i(LOG_TAG, "Decreased from 0 volume. Value remains the same. RINGER");
+            assertEquals(beforeValue, afterValue);
+        }
+    }
+
+    @Test
+    public void testRingerVolumeIncrease() {
+        Intent testIntent = new Intent(appContext, AdjustVolumeReceiver.class);
+
+        testIntent.putExtra(AdjustVolumeReceiver.VOLUME_TYPE_EXTRA, AdjustVolumeReceiver.RING_VOLUME);
+        testIntent.putExtra(AdjustVolumeReceiver.VOLUME_DIRECTION, AdjustVolumeReceiver.VOLUME_INCREASE);
+
+        AudioManager audioManager = (AudioManager) appContext.getSystemService(Context.AUDIO_SERVICE);
+
+        int beforeValue = audioManager.getStreamVolume(AudioManager.STREAM_RING);
+
+        appContext.sendBroadcast(testIntent);
+
+        for(int i = 0; i < 20; i++){
+            appContext.sendBroadcast(testIntent);
+        }
+
+        try{
+            Thread.sleep(3000); // Delay
+        }catch(InterruptedException e){
+            Log.e(LOG_TAG, e.getMessage(), e);
+        }
+
+        int afterValue = audioManager.getStreamVolume(AudioManager.STREAM_RING);
+
+        if(beforeValue < audioManager.getStreamMaxVolume(AudioManager.STREAM_RING)) {
+            Log.i(LOG_TAG, "Increased from " + beforeValue + " to " + afterValue + " RINGER");
+            assertTrue(beforeValue < afterValue);
+        }else{
+            Log.i(LOG_TAG, "Increased from max volume. Value remains the same. RINGER");
             assertEquals(beforeValue, afterValue);
         }
     }
