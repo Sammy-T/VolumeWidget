bb7493a5aa0cfdd7dbecfb7937fcf8b61c7dba35
Sammy-T <cyam9019@aim.com>
2020-08-29 11:47:30 -0400

    Update target version and dependencies

--- app/src/androidTest/java/sammyt/volumewidget/VolumeReceiverTest.java
+++ app/src/androidTest/java/sammyt/volumewidget/VolumeReceiverTest.java
@@ -1,15 +1,12 @@
 package sammyt.volumewidget;
 
-import android.app.Instrumentation;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import android.util.Log;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
--- app/src/main/java/sammyt/volumewidget/MainActivity.java
+++ app/src/main/java/sammyt/volumewidget/MainActivity.java
@@ -1,6 +1,6 @@
 package sammyt.volumewidget;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
